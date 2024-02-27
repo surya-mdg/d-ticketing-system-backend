d2e81f999c73cf7b9e439bf2cd59d5a2dfc086c6
Surya <72032559+surya-mdg@users.noreply.github.com>
2024-02-27 17:22:31 +0530

    safe version

--- src/Ticket.sol
+++ src/Ticket.sol
@@ -8,19 +8,80 @@ contract Ticket is ERC721URIStorage{
     uint256 tokenId = 0;
     mapping(uint256 => Event) events;
 
+    event TokenMinted(address indexed owner, uint256 indexed tokenId);
+    event TokenTransferred(address indexed from, address indexed to, uint256 indexed tokenId);
+    event TokenSold(address indexed buyer, uint256 indexed tokenId);
+
     struct Event{
-        string name;
-        string location;
+        address payable owner;
         uint256 time;
+        uint256 transferDeadline;
+        uint256 cost;
+        uint256 sellingPrice;
+        bool isSellable;
     }
 
     constructor() ERC721("DTicket","DTK") {}
 
-    function mint() public {
+    modifier onlyTokenOwner(uint256 _tokenId){
+        require(msg.sender == ERC721.ownerOf(_tokenId), "Error: Function caller not the token owner");
+        _;
+    }
+
+    modifier tokenExists(uint256 _tokenId){
+        require(_tokenId < tokenId, "Error: Token ID does not exist");
+        _;
+    }
+
+    function mint(string memory _uri, uint256 _eventTime, uint8 _transferType, uint256 _deadlineTime, uint256 _ticketCost) public {
+        require(_eventTime > block.timestamp, "Mint Error: Event time needs to be greater than current time");
+
         _mint(msg.sender, tokenId);
-        _setTokenURI()
+        _setTokenURI(tokenId, _uri);
+
+        if(_transferType == 0)
+            events[tokenId] = Event(payable(msg.sender), _eventTime, _eventTime, _ticketCost, _ticketCost, false);
+        else if(_transferType == 1)
+            events[tokenId] = Event(payable(msg.sender), _eventTime, _eventTime - _deadlineTime, _ticketCost, _ticketCost, false);
+        else
+            events[tokenId] = Event(payable(msg.sender), _eventTime, 0, _ticketCost, _ticketCost, false);
+
+        emit TokenMinted(msg.sender, tokenId);
         tokenId++;
     }
 
+    function transfer(uint256 _tokenId, address _to) tokenExists(_tokenId) tokenExists(_tokenId) onlyTokenOwner(_tokenId) public{
+        require(block.timestamp < events[_tokenId].transferDeadline, "Transfer Error: Cannot transfer token after deadline");
 
+        _transfer(msg.sender, _to, _tokenId);
+        emit TokenTransferred(msg.sender, _to, _tokenId);
+    }
+
+    function verify(uint256 _tokenId, address _owner) tokenExists(_tokenId) view public returns(bool){
+        return ERC721.ownerOf(_tokenId) == _owner;
+    }
+
+    function sell(uint256 _sellingPrice, uint256 _tokenId) tokenExists(_tokenId) onlyTokenOwner(_tokenId) public {
+        require(_sellingPrice <= events[_tokenId].cost, "Sell Error: Selling price cannot be greater than initial cost");
+        require(block.timestamp <= events[_tokenId].transferDeadline, "Sell Error: Cannot transfer token after deadline");
+
+        events[_tokenId].sellingPrice = _sellingPrice;
+        events[_tokenId].isSellable = true;
+    }
+
+    function revertSell(uint256 _tokenId) tokenExists(_tokenId) onlyTokenOwner(_tokenId) external {
+        events[_tokenId].isSellable = false;
+    }
+
+    function buy(uint256 _tokenId) tokenExists(_tokenId) external payable{
+        require(events[_tokenId].isSellable, "Buy Error: Token not for sale");
+        require(block.timestamp <= events[_tokenId].transferDeadline, "Buy Error: Cannot transfer token after deadline");
+        require(msg.value >= events[_tokenId].sellingPrice, "Buy Error: Insuffiecient amount sent");
+
+        events[_tokenId].isSellable = false;
+        _burn(_tokenId);
+        _mint(msg.sender, _tokenId);
+        events[_tokenId].owner.transfer(events[_tokenId].sellingPrice);
+        emit TokenSold(msg.sender, _tokenId);
+    }
 }
---
+++ test/Ticket.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Ticket} from "../src/Ticket.sol";
+
+contract TicketTest is Test {
+    Ticket public ticket;
+    address owner = vm.addr(1);
+    address user1 = vm.addr(2);
+    address user2 = vm.addr(3);
+
+    event TokenMinted(address indexed owner, uint256 indexed tokenId);
+    event TokenTransferred(address indexed from, address indexed to, uint256 indexed tokenId);
+    event TokenSold(address indexed buyer, uint256 indexed tokenId);
+
+    function setUp() public {
+        ticket = new Ticket();
+        vm.deal(user1, 10 ether);
+        vm.deal(user2, 10 ether);
+
+        vm.startPrank(user1);
+        ticket.mint("https://www.google.com", block.timestamp + 120, 0, 0, 2);
+        ticket.mint("https://www.yahoo.com", block.timestamp + 120, 1, 120, 2);
+        ticket.mint("https://www.hotmail.com", block.timestamp + 120, 2, 0, 2);
+        vm.stopPrank();
+    }
+
+    function test_Mint() public{
+        vm.startPrank(user1);
+        vm.expectEmit(true, true, false, false);
+        emit TokenMinted(user1, 3);
+        ticket.mint("https://www.google.com", block.timestamp + 120, 0, 0, 1);
+
+        vm.expectRevert("Mint Error: Event time needs to be greater than current time");
+        ticket.mint("https://www.google.com", 1, 0, 0, 1);
+    }
+
+    function test_Sell() public{
+        vm.startPrank(user1);
+        vm.expectRevert("Sell Error: Selling price cannot be greater than initial cost");
+        ticket.sell(3, 0);
+
+        //vm.expectRevert("Sell Error: Cannot transfer token after deadline");
+        ticket.sell(1, 1);
+
+        vm.expectRevert("Sell Error: Cannot transfer token after deadline");
+        ticket.sell(1, 2);
+    }
+
+    function test_SellBuy() public{
+        vm.prank(user1);
+        ticket.sell(1, 0);
+
+        vm.prank(user2);
+        //vm.expectEmit(true, true, false, false);
+        //emit TokenSold(user2, 0);
+        ticket.buy{value: 3}(0);
+
+        assertEq(ticket.verify(0, user1), false);
+        assertEq(ticket.verify(0, user2), true);
+
+        //vm.prank(user1);
+        //ticket.transfer(0);
+
+        //assertEq(ticket.verify(0, user2), true);
+    }
+
+
+}
